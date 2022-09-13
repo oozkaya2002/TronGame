14b34664ca42cf204dc3052be42d4cf96bfff696
oozkaya2002 <98242412+oozkaya2002@users.noreply.github.com>
2022-09-13 14:45:08 -0500

    Uploaded Files for Tron Game

---
+++ C++ Final Project/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/oozka/OneDrive/Desktop/School + Personal/Courses/Comp Sci/CS 211/C++ Final Project
+# Build directory: C:/Users/oozka/OneDrive/Desktop/School + Personal/Courses/Comp Sci/CS 211/C++ Final Project/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test_model_test "model_test")
+set_tests_properties(Test_model_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/oozka/OneDrive/Desktop/School + Personal/Courses/Comp Sci/CS 211/C++ Final Project/.cs211/cmake/211commands.cmake;90;add_test;C:/Users/oozka/OneDrive/Desktop/School + Personal/Courses/Comp Sci/CS 211/C++ Final Project/CMakeLists.txt;22;add_test_program;C:/Users/oozka/OneDrive/Desktop/School + Personal/Courses/Comp Sci/CS 211/C++ Final Project/CMakeLists.txt;0;")
+subdirs(".cs211/lib/catch")
+subdirs(".cs211/lib/ge211")
